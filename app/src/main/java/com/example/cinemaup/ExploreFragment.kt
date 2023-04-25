91e3091de9a1373f561841bb0cf49d7338791df2
tanveerm176 <tanveerm176@gmail.com>
2023-04-24 22:23:47 -0400

    Add explore fragment

--- app/src/main/java/com/example/cinemaup/ExploreFragment.kt
+++ app/src/main/java/com/example/cinemaup/ExploreFragment.kt
@@ -1,8 +1,11 @@
 package com.example.cinemaup
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,15 +16,20 @@ import kotlinx.serialization.json.Json
 import okhttp3.Headers
 import org.json.JSONException
 
+fun createJson() = Json {
+    isLenient = true
+    ignoreUnknownKeys = true
+    useAlternativeNames = false
+}
 
+private const val TAG = "MainActivity/"
 
-private const val TAG = "MainActivity"
-
-private const val NowPlaying_MOVIE_SEARCH_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=${BuildConfig.API_KEY}&language=en-US&page=1.json"
-private const val Upcoming_MOVIE_SEARCH_URL = "https://api.themoviedb.org/3/movie/upcoming?api_key=${BuildConfig.API_KEY}&language=en-US&page=1.json"
+private const val SEARCH_API_KEY = "a07e22bc18f5cb106bfe4cc1f83ad8ed"//BuildConfig.API_KEY
+private const val NowPlaying_MOVIE_SEARCH_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=${SEARCH_API_KEY}&language=en-US&page=1.json"
+private const val Upcoming_MOVIE_SEARCH_URL = "https://api.themoviedb.org/3/movie/upcoming?api_key=${SEARCH_API_KEY}&language=en-US&page=1.json"
 //    "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${SEARCH_API_KEY}"
 
-class MainActivity : AppCompatActivity() {
+class ExploreFragment : Fragment() {
 
     private val nowPlayingList = mutableListOf<Movie>()
     private val upcomingList = mutableListOf<Movie>()
@@ -29,16 +37,14 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var nowPlayingRV: RecyclerView
     private lateinit var upcomingRV: RecyclerView
-
     private lateinit var binding: ActivityMainBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-
-        //note: Top Recycler View layout and binding-------------
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_explore, container, false)
         nowPlayingRV = findViewById(R.id.nowPlayingRV)
         val nowPlayingAdapter = MovieAdapter(this, nowPlayingList)
         nowPlayingRV.adapter = nowPlayingAdapter
@@ -50,11 +56,11 @@ class MainActivity : AppCompatActivity() {
 
         //note: Bottom Recycler View layout and binding-------------
         upcomingRV = findViewById(R.id.upcomingRV)
-        val upcomingAdapter = MovieAdapter(this, upcomingList)
+        val upcomingAdapter = MovieAdapter(view.context, upcomingList)
         upcomingRV.adapter = upcomingAdapter
 
-        upcomingRV.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false).also {
-            val dividerItemDecoration = DividerItemDecoration(this, it.orientation)
+        upcomingRV.layoutManager = LinearLayoutManager(view.context, LinearLayoutManager.HORIZONTAL, false).also {
+            val dividerItemDecoration = DividerItemDecoration(view.context, it.orientation)
             upcomingRV.addItemDecoration(dividerItemDecoration)
         }
 
@@ -62,18 +68,20 @@ class MainActivity : AppCompatActivity() {
         val client = AsyncHttpClient()
 
         //note: now playing recycler view API call
-        client.get(NowPlaying_MOVIE_SEARCH_URL, ResponseHandler(nowPlayingList, nowPlayingAdapter))
+        client.get(NowPlaying_MOVIE_SEARCH_URL,
+            ResponseHandler(nowPlayingList, nowPlayingAdapter)
+        )
 
         //note: upcoming recycler view API call
-        client.get(Upcoming_MOVIE_SEARCH_URL, ResponseHandler(upcomingList, upcomingAdapter))
+        client.get(Upcoming_MOVIE_SEARCH_URL,
+            ResponseHandler(upcomingList, upcomingAdapter)
+        )
         Log.d(TAG, "${nowPlayingList.size}")
-
-
+        return view
     }
 
-
     private class ResponseHandler(private val movieList: MutableList<Movie>, private val adapter: MovieAdapter) :
-        JsonHttpResponseHandler() {
+    JsonHttpResponseHandler() {
         override fun onFailure(
             statusCode: Int,
             headers: Headers?,
@@ -83,7 +91,7 @@ class MainActivity : AppCompatActivity() {
             Log.e(TAG, "Failed to fetch movies: $statusCode")
         }
 
-        override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
+        override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
 
             Log.i(TAG, "Successfully fetched movies: $json")
             try {
@@ -114,4 +122,5 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
 }
